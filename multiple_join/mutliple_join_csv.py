88a50ef2c3d2b96dfa2944cea2f457be4d07c288
tais <tgrippa@ulb.ac.be>
2018-01-26 15:11:16 +0100

    rename leftjoin to multiplejoin and adapt function to allow both left join and inner join

--- multiple_join/mutliple_join_csv.py
+++ multiple_join/mutliple_join_csv.py
@@ -1,29 +1,21 @@
-## Function which execute a left join using individual .csv files.
-## This ddddddddddddd
-# The argument "indir" wait for a string containing the path to the directory where the individual .csv files are stored.
-# The argument "outfile" wait for a string containing the path to the output file to create.
-# The argument "overwrite" wait for True/False value allow or not to overwrite existing outfile.
-# The argument "pattern" wait for a string containing the pattern of filename to use. Use wildcards is possible (*.csv for all .csv files)
-
-
 def atoi(text):
     '''
     Function that return integer if text is digit - Used in 'natural_keys' function
     '''
     return int(text) if text.isdigit() else text
 
-def natural_keys(text):   #Return key to be used for sorting string containing numerical values - Used in 'join_csv' function
+def natural_keys(text):   #     Trick was found here: https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
     '''
-    Trick was found here
-    https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
+    Return key to be used for sorting string containing numerical values - Used in 'leftjoin_2csv' function
     '''
     import re  #Import needed library
     return [ atoi(c) for c in re.split('(\d+)', text) ]  #Split the string
 
-def leftjoin_2csv(file1,file2,separator=";"):
+def join_2csv(file1,file2,separator=";",join='inner'):
+    '''
+    Function that join two csv files according to the first column (primary key).
+    'join' parameter wait either for 'left' or 'inner' according to type of join
+    '''
     import tempfile,csv,os
     header_list=[]
     file1_values_dict={}
@@ -33,11 +25,16 @@ def leftjoin_2csv(file1,file2,separator=";"):
     # Make a list of headers
     header_list1=[ x for x in reader1.next()]
     header_list2=[ x for x in reader2.next()[1:]]
-    # Make a list of unique IDs from the first and second table
-    id_list=[ row[0] for row in reader1]
-    [id_list.append(row[0]) for row in reader2]
-    id_list=list(set(id_list))
-    id_list.sort(key=natural_keys)
+    # Make a list of unique IDs from the first and second table according to type of join
+    if join=='inner':
+        id_list=[ row[0] for row in reader1]
+        [id_list.append(row[0]) for row in reader2]
+        id_list=list(set(id_list))
+        id_list.sort(key=natural_keys)
+    if join=='left':
+        id_list=[ row[0] for row in reader1]
+        id_list=list(set(id_list))
+        id_list.sort(key=natural_keys)
     # Build dictionnary for values of file 1
     reader1=csv.reader(open(file1), delimiter=separator)
     reader1.next()
@@ -67,32 +64,40 @@ def leftjoin_2csv(file1,file2,separator=";"):
     writer.writerows(new_content) #Write multiples rows in the file
     return outfile
 
-def leftjoin_multiplecsv(fileList,outfile,separator=";",overwrite=False):
+def join_multiplecsv(fileList,outfile,separator=";",join='left',overwrite=False):
+    '''
+    Function that apply join on multiple csv files
+    '''
     import os, sys, shutil
     # Stop execution if outputfile exitst and can not be overwriten
     if os.path.isfile(outfile) and overwrite==False:
         print "File '%s' aleady exists and overwrite option is not enabled."%outfile
     else:
         if os.path.isfile(outfile) and overwrite==True:  # If outputfile exitst and can be overwriten
-            os.remove(outfile)
-            print "File '%s' has been overwrited."%outfile
+            #os.remove(outfile)
+            print "File '%s' will be overwrited."%outfile
         nbfile=len(fileList)
         if nbfile<=1: #Check if there are at least 2 files in the list
             sys.exit("This function require at least two .csv files to be jointed together.")
+        # Copy the list of file in a queue list
+        queue_list=list(fileList)
         # Left join on the two first files
-        file1=fileList.pop(0)
-        file2=fileList.pop(0)
-        tmp_file=leftjoin_2csv(file1,file2,separator=separator)
+        file1=queue_list.pop(0)
+        file2=queue_list.pop(0)
+        tmp_file=join_2csv(file1,file2,separator=separator,join=join)
         # Left join on the rest of the files in the list
-        while len(fileList)>0:
-            file2=fileList.pop(0)
-            tmp_file=leftjoin_2csv(tmp_file,file2,separator=separator)
+        while len(queue_list)>0:
+            file2=queue_list.pop(0)
+            tmp_file=join_2csv(tmp_file,file2,separator=separator,join=join)
         #Copy the temporary file to the desired output path
         shutil.copy2(tmp_file,outfile)
         # Print what happend
         print "%s individual .csv files were joint together."%nbfile
 
 def create_csvt(csv_file,separator=";",first_col_type="Integer",rest_type="Real"):
+    '''
+    Function that create a .csvt file with the same type of all columns except first one
+    '''
     import csv
     writer=csv.writer(open(csv_file+"t","w"),delimiter=separator)
     reader=csv.reader(open(csv_file,"r"),delimiter=separator)
@@ -104,8 +109,8 @@ def create_csvt(csv_file,separator=";",first_col_type="Integer",rest_type="Real"
     writer.writerow(typecolumn)
 
 # Test
+import os,sys
 test_folder=os.path.join(os.path.dirname(sys.argv[0]),"test")
-outfile=os.path.join(test_folder,"result.csv")
 fileList=[]
 fileList.append(os.path.join(test_folder,"a"))
 fileList.append(os.path.join(test_folder,"b"))
@@ -114,5 +119,12 @@ fileList.append(os.path.join(test_folder,"d"))
 fileList.append(os.path.join(test_folder,"e"))
 fileList.append(os.path.join(test_folder,"f"))
 
-leftjoin_multiplecsv(fileList,outfile,separator="|",overwrite=True)
+# Inner join
+outfile=os.path.join(test_folder,"result_inner.csv")
+join_multiplecsv(fileList,outfile,separator="|",join='inner',overwrite=True)
+create_csvt(outfile,separator="|",first_col_type="Integer",rest_type="Real")
+
+# Left join
+outfile=os.path.join(test_folder,"result_left.csv")
+join_multiplecsv(fileList,outfile,separator="|",join='left',overwrite=True)
 create_csvt(outfile,separator="|",first_col_type="Integer",rest_type="Real")
